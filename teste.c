88ea05d164e7beddb4f3abd72c5130802eff4202
Lucas <lucasgualtieri.dev@gmail.com>
2023-04-27 11:36:47 -0300

    comiting teste.c as as test

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include "C:\Users\1431578\Documents\CC-PUC\CC-PUC\Bibliotecas\biblioteca_c.h"
+
+int main() {
+
+	puts("Olá mundo!");
+
+	String string = getstr();
+
+	printf("string: %s\n", string);
+
+	puts("\n******* | FIM DO PROGRAMA | *******\n");
+	return 0;
+}
