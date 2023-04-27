80fd905dd357adadda403f63989f3ec61fd40a08
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-04-26 21:01:07 -0300

    .

--- AEDs/AEDs_I/AulasLab/AulaLab_1/questao_1.c
+++ AEDs/AEDs_I/AulasLab/AulaLab_1/questao_1.c
@@ -7,21 +7,21 @@ Autor - Lucas Gualtieri
 
 int main(void) {
 
-  int numero = 10, fator = 3;
+	int numero = 10, fator = 3;
 
-  printf("\nHello World\n\n");
-  printf("O valor salvo na variável 'número' é %d\n", numero);
-  printf("O resultado do cálculo (numero + 7) é %d\n", (numero + 7));
-  printf("O resultado do cálculo (numero * fator) é %d\n", (numero * fator));
-  printf("O valor salvo é %d na variável 'número' e %d na variável 'fator'\n", numero, fator);
+	printf("\nHello World\n\n");
+	printf("O valor salvo na variável 'número' é %d\n", numero);
+	printf("O resultado do cálculo (numero + 7) é %d\n", (numero + 7));
+	printf("O resultado do cálculo (numero * fator) é %d\n", (numero * fator));
+	printf("O valor salvo é %d na variável 'número' e %d na variável 'fator'\n", numero, fator);
 
-  int a = 10, b = 20, c = 30;
-  printf("\nA soma de A + B + C é %d\n\n", (a + b + c)); // A soma funciona sem os () mas é bom manter bons hábitos
+	int a = 10, b = 20, c = 30;
+	printf("\nA soma de A + B + C é %d\n\n", (a + b + c)); // A soma funciona sem os () mas é bom manter bons hábitos
 
-  // Loop printf, 3 repetições
-  for (int repeat = 0; repeat < 3; repeat++) {
-    printf("Hello World\n");
-  };
+	// Loop printf, 3 repetições
+	for (int repeat = 0; repeat < 3; repeat++) {
+		printf("Hello World\n");
+	};
 
-  return 0;
+	return 0;
 }
--- AEDs/AEDs_I/AulasLab/AulaLab_1/questao_2.c
+++ AEDs/AEDs_I/AulasLab/AulaLab_1/questao_2.c
@@ -2,11 +2,11 @@
 
 int main(void) {
 	int nota1, nota2, nota3, media;
-	
+
 	scanf("%d %d %d", &nota1, &nota2, &nota3);
-	
+
 	media = (nota1 + nota2 + nota3) / 3;
 	printf("A média é: %d", media);
-	
+
 	return 0;
 }
