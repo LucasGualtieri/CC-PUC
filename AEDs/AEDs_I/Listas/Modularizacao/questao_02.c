d6ebafe10149971d00d819d120bd3145a0dd2bef
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-03 20:20:13 -0300

    .

--- AEDs/AEDs_I/Listas/Modularizacao/questao_02.c
+++ AEDs/AEDs_I/Listas/Modularizacao/questao_02.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-// Essa função recebe um número inteiro n e retorna o n-ésimo termo da sequência { 2, 4, 6, 8, 10, 12... }
+/*
+	Essa função recebe um número inteiro n e retorna o
+	n-ésimo termo da sequência { 2, 4, 6, 8, 10, 12... }
+*/
 int par(int n) {
 	return n * 2;
 }
 
-// Essa função recebe um número inteiro n e mostra na tela (em ordem decrescente) ->
-// todos os valores menores do que par(n) para a sequência { 2, 4, 6, 8, 10, 12... }
+/*
+	Essa função recebe um número inteiro n e mostra na tela
+	(em ordem decrescente) todos os valores menores do que
+	par(n) para a sequência { 2, 4, 6, 8, 10, 12... }.
+	Para n = 5 -> { 8, 6, 4, 2 }
+*/
 void MostrarParOrdemDecre(int n) {
-	printf("\n{");
-	for (int i = par(n) - 2; i >= 0; i -= 2) {
 
-		printf(" %d", i);
-		if (i != 0) printf(",");
+	printf("\n{ ");
+	for (int i = par(n - 1); i > 0; i -= 2) {
+		printf("%d", i);
+		printf("%s", (i > 2) ? ", " : " }");
 	}
-	printf(" }");
 }
 
 // Esse procedimento lê o valor de n e chama a função.
@@ -34,3 +40,20 @@ int main() {
 	printf("\n\n******* | FIM DO PROGRAMA | *******\n\n");
 	return 0;
 }
+
+/*
+	// Alternativa
+	void MostrarParOrdemDecre(int n) {
+
+		int aux = Par(n);
+
+		printf("\n{ ");
+		for (int i = n; i > 0; i--) {
+			int par = Par(i);
+			if (par < aux) {
+				printf("%d", par);
+				printf("%s", (par > 2) ? ", " : " }");
+			}
+		}
+	}
+*/
