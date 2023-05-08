a7d3aea645149092c0f5b60fc92d9ef6fc1e614a
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-08 17:06:31 -0300

    .

--- AEDs/AEDs_I/MonitoriaAEDs_I/Ponteiros/introducaoPonteiros.c
+++ AEDs/AEDs_I/MonitoriaAEDs_I/Ponteiros/introducaoPonteiros.c
@@ -1,15 +1,56 @@
 #include <biblioteca_c.h>
 
-void funcao(int* ptrIdade) {
-	*ptrIdade = 20;
+void alteraValor(int* ponteiro) {
+	*ponteiro = 18;
 }
 
 int main() {
 
-	// Ponteiro é uma variável que armazena endereço de memória
 	int idade;
-	funcao(&idade);
+	int* ponteiro;
+
+	idade = 20;
+	ponteiro = &idade;
+
+	if (ponteiro == &idade) {
+		printf("ponteiro = &idade\n");
+		printf("%p = %p\n", ponteiro, &idade);
+	}
+
+	printf("---------------------------------\n");
+
+	if (*ponteiro == idade) {
+		printf("*ponteiro = idade\n");
+		printf("%d = %d\n", *ponteiro, idade);
+	}
+
+	printf("---------------------------------\n");
+
+	if (*ponteiro == *&idade) {
+		printf("*ponteiro = *&idade\n");
+		printf("%d = %d\n", *ponteiro, *&idade);
+	}
+
+	printf("---------------------------------\n");
+
+	// É necessario essa conversão, pois são endereços de tipos diferentes.
+	if ((int*)&ponteiro != &idade) { // &ponteiro != (int**)&idade
+		printf("&ponteiro != &idade\n");
+		printf("%p != %p\n", &ponteiro, &idade);
+	}
+
+	printf("---------------------------------\n");
+
+	alteraValor(&idade);
+
 	printf("Idade: %d\n", idade);
+
+	printf("---------------------------------\n");
+
+	int x = 25;
+	ponteiro = &x;
+
+	printf("X: %d\n", *ponteiro);
 
 	puts("\n******* | FIM DO PROGRAMA | *******\n");
 	return 0;
