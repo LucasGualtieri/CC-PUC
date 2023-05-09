c5c4ba264c8c8bf0862c937f799dea1f7b86c46d
Lucas Gualtieri <42350002+LucasGualtieri@users.noreply.github.com>
2023-05-09 09:55:20 -0300

    Update questao_1.c

--- AEDs/AEDs_I/MonitoriaAEDs_I/Vetor/questao_1.c
+++ AEDs/AEDs_I/MonitoriaAEDs_I/Vetor/questao_1.c
@@ -1,28 +1,27 @@
 #include <biblioteca_c.h>
 
 void AtribuindoDiasAosMeses(int meses[]) {
-	for (int i = 0; i < 12; i++) {
-		if (i < 7) {
-			if (i % 2 == 0) {
-				// Se o mês for par -> atribuímos 30
+	for (int i = 1; i <= 12; i++) {
+		if (i < 8) {
+			if (i == 2) {
+				// Se o mês for Fevereiro -> atribuímos 28
+				meses[i] = 28;
+			} else if (i % 2 != 0) {
+				// Se o mês for ímpar -> atribuímos 31
 				meses[i] = 31;
 			} else {
-				// Se o mês for ímpar -> atribuímos 31
+				// Se o mês for par -> atribuímos 30
 				meses[i] = 30;
 			}
 		} else {
-			// A partir do mês 7 a lógica muda
-			if (i % 2 != 0) {
-				// Se o mês for ímpar -> atribuímos 31
+			// A partir do mês 8 a lógica muda
+			if (i % 2 == 0) {
+				// Se o mês for par -> atribuímos 31
 				meses[i] = 31;
 			} else {
-				// Se o mês for par -> atribuímos 30
+				// Se o mês for ímpar -> atribuímos 30
 				meses[i] = 30;
 			}
-		}
-		if (i == 1) {
-			// Se o mês for Fevereiro -> atribuímos 28
-			meses[i] = 28;
 		}
 	}
 }
