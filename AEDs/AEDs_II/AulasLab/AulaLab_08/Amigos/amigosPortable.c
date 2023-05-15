720b904bc4192bdd2ad4e754309b587492293ab5
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-14 22:05:32 -0300

    Commiting amigosPortable.c

--- AEDs/AEDs_II/AulasLab/AulaLab_08/Amigos/amigosPortable.c
+++ AEDs/AEDs_II/AulasLab/AulaLab_08/Amigos/amigosPortable.c
@@ -1,11 +1,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef char* String;
+#define MaxStringLength 2000
+#define ends 2000
 
 // clear && gcc trinca.c -lm -I /home/lucas/CC-PUC/Bibliotecas -o trinca.exe && ./trinca.exe < pub.in > out.txt
 // clear && gcc trinca.c -lm -o trinca.exe && ./trinca.exe < pub.in > out.txt
+
+char* getstr(FILE* stream);
+int stringsArrayLen(String* listaAtual);
+void freeStringArray(String* arrayOfStrings);
+char** split(char* string, char* regex, bool freeBuffer);
+char* substr(char* string, int beginning, int end);
+int indexOf(char* string, char* reference, int occurrence);
+bool flushStdin(FILE* stream);
 
 void printLista(String* lista, String character) {
 	int listaLength = stringsArrayLen(lista);
@@ -42,6 +53,49 @@ int main() {
 
 	puts("\n******* | FIM DO PROGRAMA | *******\n");
 	return 0;
+}
+
+bool flushStdin(FILE* stream) {
+
+	// scanf("%d%*c", &idade);
+
+	int c = fgetc(stream);
+
+	if (c != '\n' && c != EOF) {
+		ungetc(c, stream);
+	}
+}
+
+char* substr(char* string, int beginning, int end) {
+	if (end < 1) return string;
+
+	if (end == ends) end = strlen(string);
+
+	int length = end - beginning;
+
+	static char strAux[MaxStringLength];
+	// char* strAux = (char*)malloc((length + 1) * sizeof(char));
+
+	for (int i = 0; i < length; i++) {
+		strAux[i] = string[beginning++];
+	}
+
+	strAux[length] = '\0';
+
+	return strAux;
+}
+
+int indexOf(char* string, char* reference, int occurrence) {
+
+	for (int i = 0; i <= strlen(string) - strlen(reference); i++) {
+		char* substring = substr(string, i, strlen(reference) + i);
+
+		if (!strcmp(substring, reference)) {
+			occurrence--;
+			if (occurrence == 0) return i;
+		}
+	}
+	return -1;
 }
 
 char* getstr(FILE* stream) {
