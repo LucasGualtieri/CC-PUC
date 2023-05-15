4c3dc88f8e505958f2c740b99682e80277508fa1
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-15 17:27:00 -0300

    .

--- AEDs/AEDs_I/MonitoriaAEDs_I/Matrizes/prova2_Semestre2_2022_Q2.c
+++ AEDs/AEDs_I/MonitoriaAEDs_I/Matrizes/prova2_Semestre2_2022_Q2.c
@@ -1,28 +1,84 @@
 #include <biblioteca_c.h>
 
-void printMatriz(int size, float matriz[size][size]) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			printf("%g%s", matriz[i][j], " ");
+#define MATRIX_SIZE 5
+
+typedef struct Matrix {
+	// matriz[ROWS][COLUMNS];
+	float** pointer;
+	int rows;
+	int columns;
+} Matrix;
+
+// Helper Functions
+void printMatriz(Matrix matrix);
+void MatrixAllocation(Matrix* matrix, int rows, int columns);
+void MatrixFillAscending(Matrix matrix);
+void MatrixFree(Matrix matrix);
+
+bool MatrixScan(Matrix matrix, int i, int j) {
+	printMatriz(matrix);
+
+	int negativosLinhai = 0, negativosColunaj = 0;
+
+	for (int k = 0; k < matrix.columns; k++) {
+		if (matrix.pointer[i][k] < 0) negativosLinhai++;
+	}
+
+	for (int k = 0; k < matrix.rows; k++) {
+		if (matrix.pointer[k][j] < 0) negativosColunaj++;
+	}
+
+	return negativosLinhai == negativosColunaj ? true : false;
+}
+
+int main() {
+	Matrix matrix;
+	MatrixAllocation(&matrix, 3, 3);
+
+	MatrixFillAscending(matrix);
+
+	printf("%s\n", MatrixScan(matrix, 0, 0) ? "True! :)" : "False! :(");
+
+	MatrixFree(matrix);
+
+	puts("\n******* | FIM DO PROGRAMA | *******\n");
+	return 0;
+}
+
+void printMatriz(Matrix matrix) {
+	for (int i = 0; i < matrix.rows; i++) {
+		for (int j = 0; j < matrix.columns; j++) {
+			printf("%g%s", matrix.pointer[i][j], " ");
 		}
 		printf("\n");
 	}
 }
 
-bool function(int size, float matriz[size][size], int i, int j) {
-	// printMatriz(size, matriz);
+void MatrixAllocation(Matrix* matrix, int rows, int columns) {
+	matrix->rows = rows;
+	matrix->columns = columns;
 
-	// int negativosLinhai = 0, negativosColunaj;
+	matrix->pointer = (float**)malloc((rows + 1) * sizeof(float*));
+	for (int i = 0; i < rows; i++) {
+		matrix->pointer[i] = (float*)malloc(columns * sizeof(float));
+	}
 }
 
-int main() {
+void MatrixFree(Matrix matrix) {
 
-	int size = 2;
-	float matriz[2][2] = { { 1, 2 }, { 3, 4 } }; // Função pra montar a matriz com malloc e size lido do teclado
-	// descobrir o que é coluna e o que é linha col lin? ou lin col?
+	for (int i = 0; i < matrix.rows; i++) {
+		free(matrix.pointer[i]);
+	}
 
-	function(size, matriz, 1, 4);
+	free(matrix.pointer);
+}
 
-	puts("\n******* | FIM DO PROGRAMA | *******\n");
-	return 0;
+void MatrixFillAscending(Matrix matrix) {
+	int contador = 0;
+	for (int i = 0; i < matrix.rows; i++) {
+		for (int j = 0; j < matrix.columns; j++) {
+			contador++;
+			matrix.pointer[i][j] = contador % 2 == 0 ? contador : -contador;
+		}
+	}
 }
