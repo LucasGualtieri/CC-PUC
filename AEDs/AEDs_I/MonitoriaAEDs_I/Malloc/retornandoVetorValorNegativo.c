318a9fd2674c504d121fcca4a4be85092a1cf24d
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-22 20:16:48 -0300

    .

--- AEDs/AEDs_I/MonitoriaAEDs_I/Malloc/retornandoVetorValorNegativo.c
+++ AEDs/AEDs_I/MonitoriaAEDs_I/Malloc/retornandoVetorValorNegativo.c
@@ -17,7 +17,7 @@ int* metodo(int vetor[], int tamanhoVetor, int* qtdNegativos) {
 	for (int i = 0; i < tamanhoVetor; i++) {
 		if (vetor[i] < 0) {
 			vetorDeNegativos[*qtdNegativos] = vetor[i];
-			*qtdNegativos = *qtdNegativos + 1;
+			*qtdNegativos += 1;
 		}
 	}
 
