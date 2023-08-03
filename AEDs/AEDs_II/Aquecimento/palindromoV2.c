34a3779181554ebb152228325a6e4105416c55ff
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-03 13:54:51 -0300

    .

--- AEDs/AEDs_II/Aquecimento/palindromoV2.c
+++ AEDs/AEDs_II/Aquecimento/palindromoV2.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #define STRING_MAX_LEN 150
 
@@ -11,9 +10,7 @@ typedef char* string;
 
 // clear && gcc palindromo.c -o palindromo && ./palindromo < pub.in > alun.out
 
-bool isSpecialChar(char c) {
-	return !(isalnum(c) || c == ' ' || c == '-');
-}
+bool isSpecialChar(char c) { return !(isalnum(c) || c == ' ' || c == '-'); }
 
 bool isPalindromo(string str) {
 
