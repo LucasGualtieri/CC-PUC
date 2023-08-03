6dad408fb6b17eefe8fbfbfc9df749077f977eb8
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-02 23:05:59 -0300

    palindromo 82%

--- AEDs/AEDs_II/Aquecimento/palindromo.c
+++ AEDs/AEDs_II/Aquecimento/palindromo.c
@@ -1,13 +1,36 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#define STRING_MAX_LEN 50
+#define UNKNOWN_CHAR_BEG -17
+#define UNKNOWN_CHAR_END -67
+
+typedef char* string;
+
+// clear && gcc palindromoFunc.c -o palindromoFunc && ./palindromoFunc < pub.in > alun.out
+
+bool isSpecialChar(char c) {
+	return !(isalnum(c) || c == ' ' || c == '-');
+}
 
 bool isPalindromo(char* str) {
+
 	int left = 0, right = strlen(str) - 1;
 
 	while (left <= right) {
-		if (str[left++] != str[right--]) return false;
+
+		if (str[left] == UNKNOWN_CHAR_BEG) {
+			if (str[right] != UNKNOWN_CHAR_END) return false;
+			left += 2, right -= 2;
+		} else if (isSpecialChar(str[left])) {
+			if (str[left++] != str[--right]) return false;
+		} else {
+			if (str[left++] != str[right--]) return false;
+		}
 	}
 
 	return true;
@@ -15,15 +38,16 @@ bool isPalindromo(char* str) {
 
 int main() {
 
-	char str[50];
+	string str = (string)malloc(STRING_MAX_LEN * sizeof(char));
 
-	scanf("%[^\n\r]%*c", str);
+	scanf("%[^\r\n]", str);
 
 	while (strcmp(str, "FIM")) {
-		printf("%s", isPalindromo(str) ? "SIM" : "NAO");
-		scanf("%[^\n\r]%*c", str);
+		printf("%s\n", isPalindromo(str) ? "SIM" : "NAO");
+		scanf(" %[^\r\n]", str);
 	}
 
-	printf("FIM DO PROGRAMA");
+	free(str);
+
 	return 0;
 }
