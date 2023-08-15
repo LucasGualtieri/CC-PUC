9e8337d333fb77d2eba1c62adb5ece09ef455bb5
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-15 09:21:00 -0300

    Estudos AEDs II

---
+++ AEDs/AEDs_II/EstudoDeComplexidade/selectionSort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Array {
+	size_t size;
+	int*   pointer;
+} Array;
+
+// arrayPrint para inteiros
+void intArrayPrint(Array array) {
+
+	printf("{ ");
+	for (int i = 0; i < array.size; i++) {
+		printf("%d", array.pointer[i]);
+		printf("%s", i < array.size - 1 ? ", " : " }\n");
+	}
+}
+
+void selectionSort(Array array) {
+	int qtdComparacoes = 0, movimentacoes = 0;
+
+	int* V = array.pointer;
+	int	 N = array.size;
+
+	for (int i = 0; i < N - 1; i++) {
+		int menor = i;
+		for (int j = i + 1; j < N; j++) {
+			qtdComparacoes++;
+			if (V[menor] > V[j]) menor = j;
+		}
+		movimentacoes += 3;
+		int swap = V[menor];
+		V[menor] = V[i];
+		V[i]	 = swap;
+	}
+
+	printf("Comparações: %d\n", qtdComparacoes);
+	printf("Movimentações: %d\n", movimentacoes);
+}
+
+int main() {
+
+	Array array;
+	array.size	  = 5;
+	array.pointer = (int*)malloc(array.size * sizeof(int));
+
+	array.pointer[0] = 5;
+	array.pointer[1] = 4;
+	array.pointer[2] = 3;
+	array.pointer[3] = 2;
+	array.pointer[4] = 1;
+
+	intArrayPrint(array);
+
+	selectionSort(array);
+
+	intArrayPrint(array);
+}
