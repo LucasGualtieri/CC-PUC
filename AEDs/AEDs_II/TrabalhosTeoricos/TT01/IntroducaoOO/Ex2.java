f54bfaa17b5d3fce202e39552025ee88db0f936a
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-21 03:37:10 -0300

    TT01 - IntroduçãoOO

---
+++ AEDs/AEDs_II/TrabalhosTeoricos/TT01/IntroducaoOO/Ex2.java
@@ -0,0 +1,37 @@
+class Ex2 {
+	static class Retangulo {
+		double base, altura;
+
+		Retangulo() {
+			this(0.0, 0.0);
+		}
+
+		Retangulo(double base, double altura) {
+			this.base = base;
+			this.altura = altura;
+		}
+
+		double getArea() {
+			return base * altura;
+		}
+
+		double getPerimetro() {
+			return base * 2 + altura * 2;
+		}
+
+		double getDiagonal() {
+			return Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2));
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Retangulo r1, r2;
+		r1 = new Retangulo(5, 10);
+		r2 = new Retangulo(4, 3);
+
+		System.out.printf("r1.area(): %.2f - r1.perim(): %.2f\n", r1.getArea(), r1.getPerimetro());
+		System.out.printf("r2.diagonal(): %.2f\n", r2.getDiagonal());
+
+	}
+}
---
+++ AEDs/AEDs_II/TrabalhosTeoricos/TT01/IntroducaoOO/Ex3.java
@@ -0,0 +1,97 @@
+class Ex3 {
+
+	static class Ponto {
+		private double x, y;
+		private int id;
+		private static int nextID = 0;
+
+		Ponto() {
+			this(0.0, 0.0);
+		}
+
+		Ponto(double x, double y) {
+			this.x = x;
+			this.y = y;
+			id = nextID;
+			nextID++;
+		}
+
+		void setX(double x) {
+			this.x = x;
+		}
+
+		double getX() {
+			return x;
+		}
+
+		void setY(double y) {
+			this.y = y;
+		}
+
+		double getY() {
+			return y;
+		}
+
+		// Method to calculate distance between this point and another point
+		double dist(Ponto other) {
+			double dx = other.x - this.x;
+			double dy = other.y - this.y;
+			return Math.sqrt(dx * dx + dy * dy);
+		}
+
+		// Method to calculate distance between this point and a given point (x, y)
+		double dist(double x, double y) {
+			double dx = x - this.x;
+			double dy = y - this.y;
+			return Math.sqrt(dx * dx + dy * dy);
+		}
+
+		// Static method to calculate distance between two points (x1, y1) and (x2, y2)
+		static double dist(double x1, double y1, double x2, double y2) {
+			double dx = x2 - x1;
+			double dy = y2 - y1;
+			return Math.sqrt(dx * dx + dy * dy);
+		}
+
+		// Static method to check if three points can form a triangle
+		static boolean isTriangulo(Ponto p1, Ponto p2, Ponto p3) {
+			double a = p1.dist(p2);
+			double b = p1.dist(p3);
+			double c = p2.dist(p3);
+			return a + b > c && a + c > b && b + c > a;
+		}
+
+		// Method to calculate the area of a rectangle formed by this point and another
+		// point
+		double getAreaRetangulo(Ponto other) {
+			double width = Math.abs(other.x - this.x);
+			double height = Math.abs(other.y - this.y);
+			return width * height;
+		}
+
+		// Method to get the ID of this point
+		int getID() {
+			return id;
+		}
+
+		// Static method to get the next available ID
+		static int getNextID() {
+			return nextID;
+		}
+	}
+
+	public static void main(String[] args) {
+		Ponto p1 = new Ponto(4, 3);
+		Ponto p2 = new Ponto(8, 5);
+		Ponto p3 = new Ponto(9.2, 10);
+		System.out.println("Distancia p1 entre e p2: " + p1.dist(p2));
+		System.out.println("Distancia p1 entre e (5,2): " + p1.dist(5, 2));
+		System.out.println("Distancia (4,3) entre e (5,2): " + Ponto.dist(4, 3, 5, 2));
+		System.out.println("P1, P2, P3 sao triangulo:" + Ponto.isTriangulo(p1, p2, p3));
+		System.out.println("Area retangulo:" + p1.getAreaRetangulo(p2));
+		System.out.println("ID de P1: " + p1.getID());
+		System.out.println("ID de P2: " + p2.getID());
+		System.out.println("ID de P3: " + p3.getID());
+		System.out.println("Next ID: " + Ponto.getNextID());
+	}
+}
