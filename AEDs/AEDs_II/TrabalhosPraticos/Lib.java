e2a4846fd12ff4895361d37b6a7c0446cd30c45d
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-21 23:54:57 -0300

    TP01 - Bibliotecas C e Java

---
+++ AEDs/AEDs_II/TrabalhosPraticos/Lib.java
@@ -0,0 +1,31 @@
+package TrabalhosPraticos;
+
+import java.util.*;
+
+public class Lib {
+
+	static Scanner scanner = new Scanner(System.in);
+
+	public static boolean isEqual(String str1, String str2) {
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) != str2.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static String getstr() {
+		String str = scanner.nextLine();
+		return str.substring(0, str.length());
+	}
+
+	public static String getstr(String msg) {
+		System.out.printf("%s: ", msg);
+		String str = scanner.nextLine();
+		return str.substring(0, str.length());
+	}
+}
