586d101c9df2bb6051748b76f130b48ed2f5cb00
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-27 18:59:06 -0300

    TP01 - Lib java

--- AEDs/AEDs_II/TrabalhosPraticos/Lib.java
+++ AEDs/AEDs_II/TrabalhosPraticos/Lib.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Lib {
 
 	static Scanner scanner = new Scanner(System.in);
-	private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")));
+	// private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")));
 
 	public static boolean isEqual(String str1, String str2) {
 		if (str1.length() != str2.length()) {
@@ -19,17 +19,17 @@ public class Lib {
 		return true;
 	}
 	
-	public static char readChar(){
-		return readInt(); // (char)readInt();
-	}
+	// public static char readChar(){
+	// 	return readInt(); // (char)readInt();
+	// }
 
-	public static int readInt() { // Talvez tenha que pegar o getChar do MyIO mais uma vez caso isso não funcione
-		char int; // char resp = ' ';
-		try {
-			resp = (char)in.read();
-		} catch(Exception e) {}
-		return resp;
-	}
+	// public static int readInt() { // Talvez tenha que pegar o getChar do MyIO mais uma vez caso isso não funcione
+	// 	char int; // char resp = ' ';
+	// 	try {
+	// 		resp = (char)in.read();
+	// 	} catch(Exception e) {}
+	// 	return resp;
+	// }
 
 	public static String getstr() {
 		String str = scanner.nextLine();
