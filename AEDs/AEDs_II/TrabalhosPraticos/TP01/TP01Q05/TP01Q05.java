9cc3f8ae4d56cfa20abef32053ca930b3f6e2bd9
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-28 16:47:37 -0300

    TP01 - Q05

--- AEDs/AEDs_II/TrabalhosPraticos/TP01/TP01Q05/TP01Q05.java
+++ AEDs/AEDs_II/TrabalhosPraticos/TP01/TP01Q05/TP01Q05.java
@@ -4,6 +4,45 @@ import TrabalhosPraticos.Lib;
 
 class TP01Q05 {
 
+Selection sort
+	//static boolean AND() {
+	//	boolean val1, boolean val2;
+	//	// val1 = searchForVal();
+	//	
+	//	// Agora buscamos os segundo valor
+	//	// val2 = searchForVal();
+
+	//	return val1 && val2;
+	//}
+
+	//static boolean OR() {
+	//	boolean val1, boolean val2;
+	//	// val1 = searchForVal();
+	//	
+	//	// Agora buscamos os segundo valor
+	//	// val2 = searchForVal();
+
+	//	return val1 || val2;
+	//}
+
+	//static boolean NOT() {
+	//	boolean val1, boolean val2;
+	//	// Buscamos saber se temos A ou b ou c, ou and or not or not
+
+	//	if (/* SOMETHING >= A && <= C */) {
+	//		val = val1;
+	//	}
+	//	else if (/* SOMETHING == AND*/) {
+	//		val = AND();
+	//	} else if (/* SOMETHING == OR*/) {
+	//		val = OR();
+	//	} else {
+	//		val = NOT();
+	//	}
+
+	//	return !val1;
+	//}
+
 	static boolean searchForVal() {
 
 		// Esse metodo inicia a busca e as chamadas recursivas
@@ -13,6 +52,8 @@ class TP01Q05 {
 
 		// This method will iterate character by character until it finds
 		// A, B, C, AND, OR, NOT.
+
+		while ()
 
 		if (/* SOMETHING >= A && <= C */) {
 			return val;
@@ -27,55 +68,24 @@ class TP01Q05 {
 
 	}
 
-	static boolean AND() {
-		boolean val1, boolean val2;
-		// val1 = searchForVal();
-		
-		// Agora buscamos os segundo valor
-		// val2 = searchForVal();
-
-		return val1 && val2;
-	}
-
-	static boolean OR() {
-		boolean val1, boolean val2;
-		// val1 = searchForVal();
-		
-		// Agora buscamos os segundo valor
-		// val2 = searchForVal();
-
-		return val1 || val2;
-	}
-
-	static boolean NOT() {
-		boolean val1, boolean val2;
-		// Buscamos saber se temos A ou b ou c, ou and or not or not
-
-		if (/* SOMETHING >= A && <= C */) {
-			val = val1;
-		}
-		else if (/* SOMETHING == AND*/) {
-			val = AND();
-		} else if (/* SOMETHING == OR*/) {
-			val = OR();
-		} else {
-			val = NOT();
-		}
-
-		return !val1;
-	}
-
 	static int algebraBooleana(String input) {
 		
-		int N = Lib.readInt();
+		int N = StringNextInt(input);
+		
 		boolean[] entradas = new boolean[N];
 
 		for (int i = 0; i < N; i++) {
-			entradas[i] = Lib.readInt();
+			entradas[i] StringNextIntk() == 1 ? true : false;
 		}
+
+		// replace all spaces with nothing
+		// if (n == 1) replace all As with value of entradas[0];
+		// if (n == 2) replace all As with value of entradas[0] && all Bs with value of entradas[1];
+		// if (n == 3) replace all As with value of entradas[0] && all Bs with value of entradas[1], all Cs with value of entradas[2];
+
+		String entradaTratada = 
 	
 		return searchForVal();
-
 	}
 
 	public static void main(String[] args) {
