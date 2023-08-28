dcdfd5a2b53afc47deaf4b4854a09dd8c29de1eb
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-28 01:53:38 -0300

    TP01 - Q13

--- AEDs/AEDs_II/TrabalhosPraticos/TP01/TP01Q13/TP01Q13.c
+++ AEDs/AEDs_II/TrabalhosPraticos/TP01/TP01Q13/TP01Q13.c
@@ -1,21 +1,35 @@
 #include "../LibCTP01.h"
 #include <time.h>
+#include <math.h>
 
 // clear && gcc TP01Q13.c -o TP01Q13 && ./TP01Q13 < pub.in > result.txt
 
-String aleatorio(String str, int* rand) {
+char randomChar() {
+	return (char)('a' + (abs(rand()) % 26));
+}
+
+String aleatorio(String str) {
 	
+	char baseChar = randomChar();
+	char newChar = randomChar();
+	
+	int len = strlen(str);
+	
+	for (int i = 0; i < len; i++) {
+		char c = str[i];
+		str[i] = c == baseChar ? newChar : c;
+	}
+
+	return str;
 }
 
 int main() {
 
 	char str[STR_MAX_LEN];
-	
-	srand(time(0));
-	int numeroAleatorio = rand();
 
+	srand(4);
 	while (!isEqual(readstr(str), "FIM")) {
-		printf("%s\n", aleatorio(str, &numeroAleatorio) ? "SIM" : "NAO");
+		printf("%s\n", aleatorio(str));
 	}
 
 	return 0;
