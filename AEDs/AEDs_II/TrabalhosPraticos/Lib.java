f06d7b878f48c6c8ad9a6bcabe168c9753732f89
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-08-29 01:20:32 -0300

    TP01 - Lib.java - Expansão dos métodos

--- AEDs/AEDs_II/TrabalhosPraticos/Lib.java
+++ AEDs/AEDs_II/TrabalhosPraticos/Lib.java
@@ -7,15 +7,77 @@ public class Lib {
 	public static Scanner scanner = new Scanner(System.in);
 	// private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")));
 
-	public static boolean isEqual(String str1, String str2) {
-		if (str1.length() != str2.length()) {
-			return false;
-		}
-		for (int i = 0; i < str1.length(); i++) {
-			if (str1.charAt(i) != str2.charAt(i)) {
-				return false;
+	public static int ctoi(char c) {
+		return c - 48;
+	}
+
+	public static boolean ctobool(char c) {
+		return c == '1' ? true : false;
+	}
+
+	public static int IndexOf(char c, String str) {
+		int index = 0;
+		int len = str.length();
+		for (int i = 0; i < len; i++) {
+			if (str.charAt(i) == c) {
+				index = i;
+				i = len;
 			}
 		}
+		return index;
+	}
+
+	public static String toString(char[] array) {
+		String aux = "";
+		for (int i = 0; i < array.length; i++) {
+			aux += array[i];
+		}
+		return aux;
+	}
+
+	public static char[] replaceAll(char baseChar, String newStr, String str) {
+		
+		String aux = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == baseChar) aux += newStr;
+			else aux += str.charAt(i);
+		}
+
+		return aux.toCharArray();
+	}
+
+	public static char[] replaceAll(char baseChar, char newChar, char[] str) {
+		
+		String aux = "";
+
+		for (int i = 0; i < str.length; i++) {
+			if (str[i] == baseChar) aux += newChar;
+			else aux += str[i];
+		}
+
+		return aux.toCharArray();
+	}
+
+	public static char[] replaceAll(char baseChar, String newStr, char[] str) {
+		
+		String aux = "";
+
+		for (int i = 0; i < str.length; i++) {
+			if (str[i] == baseChar) aux += newStr;
+			else aux += str[i];
+		}
+
+		return aux.toCharArray();
+	}
+
+	public static boolean isEqual(String str1, String str2) {
+		if (str1.length() != str2.length()) return false;
+
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) != str2.charAt(i)) return false;
+		}
+
 		return true;
 	}
 	
