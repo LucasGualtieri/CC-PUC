1d73cfd936a7e824640c16ba115caff9ab893170
Lucas Gualtieri <42350002+LucasGualtieri@users.noreply.github.com>
2023-09-04 21:57:12 +0000

    TADs Fila circular

--- AEDs/AEDs_II/TADs/FilaCircular/filaCircular.h
+++ AEDs/AEDs_II/TADs/FilaCircular/filaCircular.h
@@ -4,7 +4,7 @@
 #include <stdbool.h> 
 
 typedef struct FilaCircular {
-	int *array, maxSize, size;
+	int *array, maxSize, first, last;
 	bool ShowOnUpdate;
 	void (*Inserir)(int number, struct FilaCircular*);
 	int (*Remover)(struct FilaCircular*);
@@ -15,29 +15,25 @@ typedef struct FilaCircular {
 } FilaCircular;
 
 void InserirFilaCircular(int number, FilaCircular* fila) {
-	if (fila->size == fila->maxSize) {
-		printf("Erro ao inserir: fila cheia.");
+	if ((fila->last + 1) % fila->maxSize == fila->first) {
+		puts("Erro ao inserir: fila cheia.");
 		return;	
 	}
-	
-	fila->array[fila->size++] = number;
+
+	fila->array[fila->last] = number;
+	fila->last = (fila->last + 1) % fila->maxSize;
 
 	if (fila->ShowOnUpdate) fila->Mostrar(*fila);
 }
 
 int RemoverFilaCircular(FilaCircular* fila) {
-	if (fila->size == 0) {
-		printf("Erro ao remover do inicio: fila vazia.");
+	if (fila->last == fila->first) {
+		puts("Erro ao inserir: fila cheia.");
 		return 0;
 	}
 
-	int removido = fila->array[0];
-
-	for (int i = 0; i < fila->size - 1; i++) {	
-		fila->array[i] = fila->array[i + 1];
-	}
-
-	fila->size--;
+	int removido = fila->array[fila->first];
+	fila->first = (fila->first + 1) % fila->maxSize;
 
 	if (fila->ShowOnUpdate) fila->Mostrar(*fila);
 
@@ -50,12 +46,15 @@ int NextFilaCircular(FilaCircular* fila) {
 
 void MostrarFilaCircular(FilaCircular fila) {
 
-	if (fila.size == 0) printf("fila vazia.\n");	
+	if (fila.maxSize == 0) {
+		printf("fila vazia.\n");	
+		return;
+	}
 
-	for (int i = 0; i < fila.size; i++) {
-		if (i == 0) printf("{ ");	
+	printf("{ ");	
+	for (int i = fila.first; i < fila.last; i = (i + 1) % fila.maxSize) {
 		printf("%d", fila.array[i]);
-		printf("%s", i < fila.size - 1 ? " " : " }\n");
+		printf("%s", i < fila.last - 1 ? " " : " }\n");
 	}
 }
 
@@ -70,8 +69,8 @@ FilaCircular newFilaCircular(size_t maxSize) {
 	if (maxSize == 0) maxSize = 80;
 	
 	fila.maxSize = maxSize;
-	fila.array = (int*)malloc(maxSize * sizeof(int));
-	fila.size = 0;
+	fila.array = (int*)malloc((maxSize + 1) * sizeof(int));
+	fila.first = fila.last = 0;
 	fila.ShowOnUpdate = false;
 	fila.ToggleShow = ToggleShowOnUpdateFilaCircular;
 	fila.Next = NextFilaCircular;
