4d9696c3944914c4769a4d62e3722bb7b175a865
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-09-07 07:20:30 -0300

    AEDs II - Prova 1 - Aquecimento Espelho

---
+++ AEDs/AEDs_II/Prova1/Treino/Espelho/Espelho.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+// clear && javac Espelho.java && java Espelho.java < pub.in > result.txt
+
+class Espelho {
+
+	static void MostrarEspelho(int li, int ls) {
+		String aux = "";
+		for (int i = li; i <= ls; i++) {
+			aux += i;
+		}
+		for (int i = aux.length() - 1; i >= 0; i--) {
+			aux += aux.charAt(i);
+		}
+		System.out.println(aux);
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while (scanner.hasNextLine()) {
+			MostrarEspelho(scanner.nextInt(), scanner.nextInt());
+			if (scanner.hasNextLine()) scanner.nextLine();
+		}
+
+		scanner.close();
+	}
+}
