edd1b6e440c582bcc9d88b9bc57e2d77fd918ebd
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-09-11 12:28:42 -0300

    prova

---
+++ ProvaPratica1/Java/Arredonda.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+// clear && javac Arredonda.java && java Arredonda < pub.in > result.txt
+
+public class Arredonda {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+		float num, cutoff;
+
+        while (scanner.hasNext()) {
+
+			num = Float.parseFloat(scanner.nextLine());
+			cutoff = Float.parseFloat(scanner.nextLine());
+
+			
+			float numF = num - (int)num;
+			float cutoffF = cutoff - (int)cutoff;
+
+			// System.out.println(fractionLen(num));
+
+			if (numF >= cutoffF) {
+				System.out.printf("%.0f\n", Math.ceil(num));
+			} else {
+				System.out.printf("%.0f\n", Math.floor(num));
+			}
+        }
+
+        scanner.close();
+    }
+}
---
+++ ProvaPratica1/Solitario/solitario.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// clear && gcc solitario.c && ./a.out < pub.in
+
+int readInt() {
+	int integer;
+	if (scanf("%d", &integer) == EOF) return 0;
+	getchar();
+	return integer;
+}
+
+void selectionSort(int* array, int n) {
+	for (int i = 0; i < n - 1; i++) {
+		int menor = i;
+		for (int j = i + 1; j < n; j++) {
+			if (array[menor] > array[j]) menor = j;
+		}
+		int swap = array[menor];
+		array[menor] = array[i];
+		array[i]	 = swap;
+	}
+}
+
+int numeroSemPar(int* array, int n) {
+	int resultado;
+	for (int i = 0; i < n; i += 2) {
+		if (i < n - 1 && array[i] != array[i + 1]) {
+			// printf("IF\n");
+			// printf("I: %d\n", i);
+			// printf("array[%d]: %d\n", i, array[i]);
+			// printf("array[%d]: %d\n", i + 1, array[i + 1]);
+			resultado = array[i];
+			i = n;
+		} else if (i == n - 1) {
+			// printf("ELSE IF\n");
+			// printf("I: %d\n", i);
+			// printf("array[%d]: %d\n", i, array[i]);
+			// printf("array[%d]: %d\n", i + 1, array[i + 1]);
+			resultado = array[i];
+		}
+	}
+	return resultado;
+}
+
+void IntArrayPrint(int* array, int n) {
+	printf("{ ");
+	for (int i = 0; i < n; i++) {
+		printf("%d", array[i]);
+		printf("%s", i < n - 1 ? ", " : " }\n");
+	}
+}
+
+int main() {
+	
+	int *array, arraySize;
+
+	while ((arraySize = readInt()) != 0) {
+		// printf("arraySize: %d\n", arraySize);
+
+		array = (int*)malloc(arraySize * sizeof(int));
+
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = readInt();
+			// printf("array[i]: %d\n", array[i]);
+			// lista.InserirOrdenado();
+		}
+
+		// IntArrayPrint(array, arraySize);
+		selectionSort(array, arraySize);
+		// IntArrayPrint(array, arraySize);
+		// exit(0);
+
+		printf("%d\n", numeroSemPar(array, arraySize));
+
+		free(array);
+		// exit(0);
+	}
+
+	return 0;
+}
