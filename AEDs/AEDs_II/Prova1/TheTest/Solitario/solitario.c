52807f5f68379e066c33be6f3cf75f31f508251d
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-09-17 11:38:44 -0300

    Prova - solitário

--- AEDs/AEDs_II/Prova1/TheTest/Solitario/solitario.c
+++ AEDs/AEDs_II/Prova1/TheTest/Solitario/solitario.c
@@ -36,7 +36,8 @@ void InsertionSort(int* array, int size) {
 		temp = array[i];
 		j = i - 1;
 		while (j >= 0 && array[j] > temp) {
-			array[j + 1] = array[j--];
+			array[j + 1] = array[j];
+			j--;
 		}
 		array[j + 1] = temp;
 		IntArrayPrint(array, size);
