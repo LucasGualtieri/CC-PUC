7946ae7c7ef48cb6264e858e665836d08b12d125
Lucas Gualtieri <42350002+LucasGualtieri@users.noreply.github.com>
2023-09-19 13:28:00 +0000

    somatório de pi

--- AEDs/AEDs_II/AnaliseComplexidade/somatorioDePi.c
+++ AEDs/AEDs_II/AnaliseComplexidade/somatorioDePi.c
@@ -4,7 +4,7 @@
 void main() {
 	float Pi, i, n = 10000;
 	for (i = 1, Pi = 0; i <= n; i++) {
-		Pi += 4 * pow(-1, i + 1) / (2 * i - 1);
+		Pi += 4 * (pow(-1, i + 1) / (2 * i - 1));
 		// if ((int)i % 2 != 0) {
 		// 	Pi += 4.0 / (2.0 * i - 1);
 		// } else {
