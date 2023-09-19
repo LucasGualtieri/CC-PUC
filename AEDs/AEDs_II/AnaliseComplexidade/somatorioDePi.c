a35b43f41282c18fb5ed81363ef5de17cfd02cfd
Lucas Gualtieri <42350002+LucasGualtieri@users.noreply.github.com>
2023-09-19 13:25:01 +0000

    somatorioDePi

---
+++ AEDs/AEDs_II/AnaliseComplexidade/somatorioDePi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+	float Pi, i, n = 10000;
+	for (i = 1, Pi = 0; i <= n; i++) {
+		Pi += 4 * pow(-1, i + 1) / (2 * i - 1);
+		// if ((int)i % 2 != 0) {
+		// 	Pi += 4.0 / (2.0 * i - 1);
+		// } else {
+		// 	Pi -= 4.0 / (2.0 * i - 1);
+		// }
+	}
+	printf("Pi: %g\n", Pi);
+}
