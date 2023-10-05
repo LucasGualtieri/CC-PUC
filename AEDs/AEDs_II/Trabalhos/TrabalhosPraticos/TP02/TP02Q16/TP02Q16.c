daacc1d593356641f93b8e0a587c2d6a078cfc4d
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-05 17:00:04 -0300

    TP02 - Q16

--- AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/TP02Q16/TP02Q16.c
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/TP02Q16/TP02Q16.c
@@ -2,7 +2,21 @@
 
 // clear && gcc TP02Q16.c && ./a.out < pub.in > result.txt
 
-void InsertionSortParcial(int k, Lista listaJogadores) {
+void registroLog(Timer timer, Resultado resultado) {
+
+	literal fileName = "794989_insercao.txt";
+	FILE* file = fopen(fileName, "w");
+
+	fprintf(file, "Matrícula: 794989\t");
+	fprintf(file, "Tempo de execução: %fs\t", timer.Time(&timer));
+	fprintf(file, "Número de comparações: %d\t", resultado.comparacoes);
+	fprintf(file, "Número de movimentações: %d", resultado.movimentacoes);
+
+	fclose(file);
+
+}
+
+void InsertionSortParcial(int k, Resultado* resultado, Lista listaJogadores) {
 	
 	int N = listaJogadores.size;
 	Jogador* array = listaJogadores.array;
@@ -10,18 +24,28 @@ void InsertionSortParcial(int k, Lista listaJogadores) {
 	int j;
 	Jogador temp;
 	for (int i = 1; i < N; i++) {
+
 		temp = array[i];
 		j = (i < k) ? i - 1 : k - 1;
 		// if (i == k) printf("Perdido: %s\n", array[k].nome);
+
+		resultado->comparacoes++;
 		while (j >= 0 && array[j].anoNascimento > temp.anoNascimento) {
 			array[j-- + 1] = array[j];
+			resultado->comparacoes++;
+			resultado->movimentacoes++;
 		}
+
 		array[j + 1] = temp;
+		resultado->movimentacoes += 2;
 	}
 }
 
 int main() {
 	
+	Timer timer = newTimer();
+	Resultado resultado = { 0, 0 };
+
 	Lista BD = newLista(BD_SIZE);
 	BD.ImportDataBase("../tmp/players.csv", &BD);
 
@@ -38,9 +62,13 @@ int main() {
 
 	int k = 10;
 
-	InsertionSortParcial(k, listaJogadores);
+	timer.Start(&timer);
+	InsertionSortParcial(k, &resultado, listaJogadores);
+	timer.Stop(&timer);
 
 	listaJogadores.MostrarParcial(k, listaJogadores);
+
+	registroLog(timer, resultado);
 
 	// listaJogadores.Close(&listaJogadores);
 	BD.Close(&BD);
