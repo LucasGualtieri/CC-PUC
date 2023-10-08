45a58ef073930fb7a947828388956e665c1d8850
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-08 03:16:43 -0300

    TP02 - Q15 - 100% & 100%

---
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/TP02Q15/TP02Q15.java
@@ -0,0 +1,57 @@
+import TP02.Libs.Lib;
+
+// clear && javac -cp ../.. TP02Q15.java && java -cp ../.. TP02Q15.java < pub.in > result.txt
+
+class TP02Q15 {
+
+	static int strcmpr(Lib.Jogador jog1, Lib.Jogador jog2) {
+		return jog1.getNome().compareTo(jog2.getNome());
+	}
+
+	static void SelectionSortParcial(int k, Lib.Lista lista, Lib.Log log) {
+
+		int i, j, menor;
+		for (i = 0; i < lista.getSize() - 1; i++) {
+			menor = i;
+			for (j = i + 1; j < k && j < lista.getSize(); j++) {
+				if (strcmpr(lista.array[menor], lista.array[j]) > 0) menor = j;
+				log.incrementarComparacoes();
+			}
+			Lib.swap(i, menor, lista.array);
+			log.incrementarMovimentacoes(3);
+		}
+
+	}
+
+	static final int BD_SIZE = 3922;
+
+	public static void main(String[] args) throws Exception {
+
+		Lib.Timer timer = new Lib.Timer();
+		Lib.Log log = new Lib.Log();
+
+		Lib.Lista BD = new Lib.Lista("../tmp/players.csv", BD_SIZE);
+		Lib.Lista listaJogadores = new Lib.Lista(465); // Tamanho de entradas do pri.in
+
+		String inputPUBIN = new String();
+		
+		while (!(inputPUBIN = Lib.readStr()).equals("FIM")) {
+			int id = Integer.parseInt(inputPUBIN);
+			listaJogadores.Inserir(BD.Get(id));
+		}
+
+	 	listaJogadores.SortByNome();
+
+		int k = 10;
+
+		timer.Start();
+		SelectionSortParcial(k, listaJogadores, log);
+		timer.Stop();
+
+		listaJogadores.MostrarParcial(k);
+
+		log.RegistroOrdenacao("794989_selecaoParcial.txt", timer);		
+
+	}
+
+}
