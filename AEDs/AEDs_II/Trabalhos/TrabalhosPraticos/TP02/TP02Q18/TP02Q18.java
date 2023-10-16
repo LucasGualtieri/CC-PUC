4c35174bea53bd996788a7a2657484bff9d79aed
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-16 20:05:28 -0300

    TP02 - Q18 - 100% & 100%

---
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/TP02Q18/TP02Q18.java
@@ -0,0 +1,56 @@
+import TP02.Libs.Lib;
+
+// clear && javac -cp ../.. TP02Q18.java && java -cp ../.. TP02Q18.java < pub.in > result.txt
+
+class TP02Q18 {
+
+	static void QuickSortRec(int k, int left, int right, Lib.Log log, Lib.Lista lista) {
+		int i = left, j = right;
+		Lib.Jogador[] array = lista.array;
+		Lib.Jogador pivot = array[left];
+		// Jogador pivot = array[(right + left) / 2];
+
+		while (i <= j) {
+			while (lista.CompareToStr(i, pivot, log) < 0) i++;
+			while (lista.CompareToStr(j, pivot, log) > 0) j--;
+			if (i <= j) lista.swap(i++, j--, log);
+		}
+
+		if (left < j) QuickSortRec(k, left, j, log, lista);
+		if (i < k && i < right) QuickSortRec(k, i, right, log, lista);
+	}
+
+	static void QuickSort(int k, Lib.Log log, Lib.Lista lista) {
+		QuickSortRec(k, 0, lista.getSize() - 1, log, lista);
+	}
+
+	static final int BD_SIZE = 3922;
+
+	public static void main(String[] args) throws Exception {
+
+		Lib.Timer timer = new Lib.Timer();
+		Lib.Log log = new Lib.Log();
+
+		Lib.Lista BD = new Lib.Lista("../tmp/players.csv", BD_SIZE);
+		Lib.Lista lista = new Lib.Lista(465); // Tamanho de entradas do pri.in
+
+		String inputPUBIN = new String();
+		
+		while (!(inputPUBIN = Lib.readStr()).equals("FIM")) {
+			int id = Integer.parseInt(inputPUBIN);
+			lista.Inserir(BD.Get(id));
+			lista.setAtributoStr(BD.Get(id).getEstadoNascimento());
+		}
+
+		int k = 10;
+
+		timer.Start();
+		QuickSort(k, log, lista);
+		timer.Stop();
+
+		lista.MostrarParcial(k);
+
+		log.RegistroOrdenacao("794989_quicksortParcial.txt", timer);
+
+	}
+}
