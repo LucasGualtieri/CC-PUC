5feec4bb82c68e0bd98b2770163fd42a0c1dd79f
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-25 20:55:29 -0300

    TP03 - Q01 - 100% & 100%

--- AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q01/TP03Q01.java
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q01/TP03Q01.java
@@ -4,19 +4,47 @@ import TP03.Libs.Lib;
 
 class TP03Q01 {
 
+	static void InserirERemover(String action, Lib.Lista lista, Lib.Lista BD) throws Exception {
+
+		String[] actions = action.split(" ");
+
+		if (actions[0].equals("II")) {
+			lista.InserirInicio(BD.Get(actions[1]));
+		} else if (actions[0].equals("IF")) {
+			lista.InserirFim(BD.Get(actions[1]));
+		} else if (actions[0].equals("I*")) {
+			lista.Inserir(actions[1], BD.Get(actions[2]));
+		} else if (actions[0].equals("RI")) {
+			System.out.printf("(R) %s\n", lista.RemoverInicio().getNome());
+		} else if (actions[0].equals("RF")) {
+			System.out.printf("(R) %s\n", lista.RemoverFim().getNome());
+		} else if (actions[0].equals("R*")) {
+			System.out.printf("(R) %s\n", lista.Remover(actions[1]).getNome());
+		}
+
+	}
+
+	static final int BD_SIZE = 3922;
+
 	public static void main(String[] args) throws Exception {
 
 		Lib.Lista BD = new Lib.Lista("../tmp/players.csv", BD_SIZE);
-		Lib.Lista listaJogadores = new Lib.Lista(200);
+		Lib.Lista lista = new Lib.Lista(250);
 
 		String inputPUBIN = new String();
 
 		while (!(inputPUBIN = Lib.readStr()).equals("FIM")) {
 			int id = Integer.parseInt(inputPUBIN);
-			listaJogadores.Inserir(BD.Get(id));
+			lista.InserirFim(BD.Get(id));
 		}
 
-		listaJogadores.Mostrar();
+		int numberOfActions = Lib.readInt();
+
+		for (int i = 0; i < numberOfActions; i++) {
+			InserirERemover(Lib.readStr(), lista, BD);
+		}
+
+		lista.Mostrar();
 
 	}
 }
