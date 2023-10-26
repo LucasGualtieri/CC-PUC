d5f619d9af9837b8edf02528a2d11afc8edb1407
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-26 17:41:35 -0300

    TP03 - Q02 - 100% & 100%

---
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q02/TP03Q02.c
@@ -0,0 +1,57 @@
+#include "../Libs/LibTP03.h"
+
+// clear && gcc TP03Q02.c && ./a.out < pub.in > result.txt
+
+void RemoverEPrintar(Jogador jogador) {
+	printf("(R) %s\n", jogador.nome);
+	jogador.Close(jogador);
+}
+
+void InserirERemover(Lista* lista, Lista BD) {
+
+	Split split = splitSpace();
+	int id = atoi(split.array[1]);
+
+	if (!strcmp(split.array[0], "II")) {
+		lista->InserirInicio(BD.Get(id, BD), lista);
+	} else if (!strcmp(split.array[0], "IF")) {
+		lista->InserirFim(BD.Get(id, BD), lista);
+	} else if (!strcmp(split.array[0], "I*")) {
+		id = atoi(split.array[2]);
+		lista->Inserir(split.array[1], BD.Get(id, BD), lista);
+	} else if (!strcmp(split.array[0], "RI")) {
+		RemoverEPrintar(lista->RemoverInicio(lista));
+	} else if (!strcmp(split.array[0], "RF")) {
+		RemoverEPrintar(lista->RemoverFim(lista));
+	} else {
+		RemoverEPrintar(lista->Remover(split.array[1], lista));
+	}
+
+}
+
+int main() {
+
+	Lista BD = newLista(BD_SIZE);
+	BD.ImportDataBase("../tmp/players.csv", &BD);
+
+	Lista lista = newLista(250); // Tamanho de entradas do pub.in
+
+	char inputPUBIN[STR_MAX_LEN];
+
+	while (strcmp(readStr(0, inputPUBIN), "FIM")) {
+		int id = atoi(inputPUBIN);
+		lista.InserirFim(BD.Get(id, BD), &lista);
+	}
+
+	int numberOfActions = readInt();
+
+	for (int i = 0; i < numberOfActions; i++) {
+		InserirERemover(&lista, BD);
+	}
+
+	lista.Mostrar(lista);
+
+	lista.Close(&lista);
+	BD.Close(&BD);
+
+}
