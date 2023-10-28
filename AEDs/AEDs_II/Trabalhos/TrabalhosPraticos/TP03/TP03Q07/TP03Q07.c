df94460e31a39c209943b173dd9ccf573ac2475a
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-27 23:22:37 -0300

    TP03 - Q07 - 100% & 100%

---
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q07/TP03Q07.c
@@ -0,0 +1,45 @@
+#include "../Libs/LibTP03.h"
+
+// clear && gcc TP03Q07.c && ./a.out < pub.in > result.txt
+
+void InserirERemover(FilaFlex* fila, Lista BD) {
+
+	Split split = splitSpace();
+
+	if (split.array[0][0] == 'I') {
+		int id = atoi(split.array[1]);
+		fila->Inserir(BD.Get(id, BD), fila);
+	} else {
+		Jogador jogador = fila->Remover(fila);
+		printf("(R) %s\n", jogador.nome);
+		jogador.Close(jogador);
+	}
+
+}
+
+int main() {
+
+	Lista BD = newLista(BD_SIZE);
+	BD.ImportDataBase("../tmp/players.csv", &BD);
+
+	FilaFlex fila = newFilaFlex(5);
+
+	char inputPUBIN[STR_MAX_LEN];
+
+	while (strcmp(readStr(0, inputPUBIN), "FIM")) {
+		int id = atoi(inputPUBIN);
+		fila.Inserir(BD.Get(id, BD), &fila);
+	}
+
+	int numberOfActions = readInt();
+
+	for (int i = 0; i < numberOfActions; i++) {
+		InserirERemover(&fila, BD);
+	}
+
+	fila.Mostrar(fila);
+
+	fila.Close(&fila);
+	BD.Close(&BD);
+
+}
--- AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q07/TP03Q07Verde.c
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q07/TP03Q07Verde.c
@@ -1,6 +1,3 @@
-#ifndef LIB_TP02_H
-#define LIB_TP02_H
-
 #include <err.h>
 #include <time.h>
 #include <stdio.h>
@@ -708,4 +705,44 @@ FilaFlex newFilaFlex(size_t maxSize) {
 	return fila;
 }
 
-#endif
+void InserirERemover(FilaFlex* fila, Lista BD) {
+
+	Split split = splitSpace();
+
+	if (split.array[0][0] == 'I') {
+		int id = atoi(split.array[1]);
+		fila->Inserir(BD.Get(id, BD), fila);
+	} else {
+		Jogador jogador = fila->Remover(fila);
+		printf("(R) %s\n", jogador.nome);
+		jogador.Close(jogador);
+	}
+
+}
+
+int main() {
+
+	Lista BD = newLista(BD_SIZE);
+	BD.ImportDataBase("/tmp/players.csv", &BD);
+
+	FilaFlex fila = newFilaFlex(5);
+
+	char inputPUBIN[STR_MAX_LEN];
+
+	while (strcmp(readStr(0, inputPUBIN), "FIM")) {
+		int id = atoi(inputPUBIN);
+		fila.Inserir(BD.Get(id, BD), &fila);
+	}
+
+	int numberOfActions = readInt();
+
+	for (int i = 0; i < numberOfActions; i++) {
+		InserirERemover(&fila, BD);
+	}
+
+	fila.Mostrar(fila);
+
+	fila.Close(&fila);
+	BD.Close(&BD);
+
+}
