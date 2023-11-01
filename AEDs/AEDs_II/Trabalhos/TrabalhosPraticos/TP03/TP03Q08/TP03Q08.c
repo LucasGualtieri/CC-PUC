6324d4a65fb259a3484b08bbeb5789b1b5e97025
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-10-31 22:45:41 -0300

    TP03 - Q08 - 100% & 100%

--- AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q08/TP03Q08.c
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP03/TP03Q08/TP03Q08.c
@@ -1,4 +1,5 @@
-#include "../Libs/LibTP03.h"
+#include "../Libs/LibsC/LibTP03.h"
+#include "../Libs/LibsC/ListaDupla.h"
 
 // clear && gcc TP03Q08.c && ./a.out < pub.in > result.txt
 
@@ -7,17 +8,19 @@ void QuickSortRec(Celula* left, Celula* right, Log* log, ListaDupla lista) {
 	Celula *pivot = left, *i = left->prox, *j = right;
 
 	while (j->prox != i) { // i <= j
-		while (!lista.CompareTo(i, pivot, log) && i != j->prox) i = i->prox;
-		while (lista.CompareTo(j, pivot, log) && j != i->ant) j = j->ant;
-		if (j->prox != i) { // i <= j
+		while (lista.CompareTo(i, pivot, log) < 0) i = i->prox;
+		while (lista.CompareTo(j, pivot, log) > 0) j = j->ant;
+		if (j->prox != i) {
  			swap(&i->jogador, &j->jogador, log);
 			i = i->prox, j = j->ant;
 		}
 	}
+
 	swap(&pivot->jogador, &j->jogador, log);
 
-	if (left != j && j != left->ant)  QuickSortRec(left, j, log, lista); // left < j
-	if (i != right && right != i->ant)  QuickSortRec(i, right, log, lista); // i < right
+	// left < j e i < right
+	if (left != j && j != left->ant) QuickSortRec(left, j, log, lista);
+	if (i != right && right->prox != i) QuickSortRec(i, right, log, lista);
 }
 
 void QuickSort(Log* log, ListaDupla lista) {
@@ -29,16 +32,16 @@ int main() {
 	Timer timer = newTimer();
 	Log log = newLog();
 
-	Lista BD = newLista(BD_SIZE);
+	BD BD = newBD(BD_SIZE);
 	BD.ImportDataBase("../tmp/players.csv", &BD);
 
-	ListaDupla lista = newListaDupla(); // Tamanho de entradas do pri.in
+	ListaDupla lista = newListaDupla();
 
 	char inputPUBIN[STR_MAX_LEN];
 
 	while (strcmp(readStr(0, inputPUBIN), "FIM")) {
 		int id = atoi(inputPUBIN);
-		lista.Inserir(BD.Get(id, BD), &lista);
+		lista.InserirFim(BD.Get(id, BD), &lista);
 	}
 
 	timer.Start(&timer);
