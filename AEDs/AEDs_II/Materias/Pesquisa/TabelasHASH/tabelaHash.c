81ba90044a897157a1d6ce3d49f0f16bae0e6c0e
Lucas Gualtieri <42350002+LucasGualtieri@users.noreply.github.com>
2023-11-14 15:35:46 +0000

    Introdução a tabelas hash

--- AEDs/AEDs_II/Materias/Pesquisa/TabelasHASH/tabelaHash.c
+++ AEDs/AEDs_II/Materias/Pesquisa/TabelasHASH/tabelaHash.c
@@ -2,39 +2,47 @@
 
 // clear && gcc tabelaHash.c && ./a.out
 
+#define TAM 7
+
 typedef struct Pessoa {
 	int id;
 	char nome[80];
 } Pessoa;
 
+#define rehash(id) ((id + 1) % TAM)
+
 void InserirHash(Pessoa pessoa, Pessoa* hash) {
-	int pos = pessoa.id % 31;
-	if (hash[pos].id != 31) hash[pos] = pessoa;
+	int pos = pessoa.id % TAM;
+	if (hash[pos].id != TAM) hash[pos] = pessoa;
+	else hash[rehash(pessoa.id)] = pessoa;
+	// else printf("Erro ao inserir %s.\n", pessoa.nome);
 }
 
 Pessoa PesquisarHash(Pessoa pessoa, Pessoa* hash) {
-	int pos = pessoa.id % 31;
+	int pos = pessoa.id % TAM;
 	return hash[pos];
 }
 
 void StartHash(Pessoa* hash) {
 	Pessoa vazia = { 0, "VAZIA" };
-	for (int i = 0; i < 31; i++) {
+	for (int i = 0; i < TAM; i++) {
 		hash[i] = vazia;
 	}
 }
 
 int main() {
 
-	Pessoa hash[31];
+	Pessoa hash[TAM];
 
-	Pessoa pessoa = { 5, "Lucas" };
+	Pessoa lucas = { 7, "Lucas" };
+	Pessoa joao = { 21, "João" };
 
 	// printf("nome: %s\n", pessoa.nome);
 
-	InserirHash(pessoa, hash);
+	InserirHash(lucas, hash);
+	InserirHash(joao, hash);
 
-	Pessoa pesquisada = PesquisarHash(pessoa, hash);
+	Pessoa pesquisada = PesquisarHash(joao, hash);
 	printf("Pessoa.nome: %s\n", pesquisada.nome);
 
 	return 0;
