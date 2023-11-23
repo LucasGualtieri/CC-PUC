0693bf084bc2bf1a94ab31cee95580a8e4d4abc0
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-11-23 16:57:13 -0300

    Misc

--- AEDs/AEDs_II/Materias/AnaliseComplexidade/SemOtimizar.c
+++ AEDs/AEDs_II/Materias/AnaliseComplexidade/SemOtimizar.c
@@ -1,32 +1,36 @@
-#include "../Libs/LibTP02.h"
+#include <stdio.h>
+#include "Timer.h"
 
-// clear && gcc Teste.c && ./a.out
-// clear && gcc Teste.c -O0 && ./a.out
-// clear && gcc Teste.c -O3 && ./a.out
+// clear && gcc SemOtimizar.c && ./a.out
+// clear && gcc SemOtimizar.c -O0 && ./a.out
+// clear && gcc SemOtimizar.c -O3 && ./a.out
 
-#define BILHAO 100000
+#define BILHAO 1000000
 
-void fn() {
+// PQ COM O BREAK É MAIS RÁPIDOOOO????
 
+double fn() {
+
+	Timer timer = newTimer();
+	timer.Start(&timer);
 	for (int i = 0; i < BILHAO; i++) {
 		for (int j = 0; j < BILHAO; j++) {
 			if (j == 5) {
-				
+				// j = BILHAO;
+				break;
 			}
-			// break;
 		}
 	}
-	
+	return timer.Stop(&timer);
 }
 
 int main() {
 
-	Timer timer = newTimer();
+	int totalIterations = 250;
+
+	double totalTime = 0;
+
+	for (int i = 0; i < totalIterations; i++) totalTime += fn();
 	
-	timer.Start(&timer);
-	fn();
-	timer.Stop(&timer);
-
-	printf("Tempo total: %lf\n", totalTime / 100);
-
+	printf("Tempo total: %lf\n", totalTime / totalIterations);
 }
