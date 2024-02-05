75c46c0fb9044a7522ed766148f632b7bfa47c93
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-02-05 18:41:50 -0300

    AEDs III - Exemplo Marketplace

---
+++ AEDs/AEDs_III/MarketPlace.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+// clear && g++ MarketPlace.cc && ./a.out
+
+using namespace std;
+
+#define extends public
+
+class Product {
+  private:
+	static int lastId;
+  protected:
+	double price;
+	string name;
+	int id;
+  public:
+	Product() {
+		this->id = Product::lastId++;
+	}
+};
+
+int Product::lastId = 0;
+
+class RubiksCube {
+  public:
+	int teste;
+};
+
+class ThreeByThree : extends Product, extends RubiksCube {
+  public:
+	int getId() { return this->id; }
+};
+
+int main() {
+	ThreeByThree p0, p1, p2, p3, p4, p5, p6;
+	p6.teste = 55;
+	printf("p6.id: %d - p6.teste: %d\n", p6.getId(), p6.teste);
+}
