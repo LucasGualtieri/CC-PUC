07522abced53844057e6dc83c5e4d51b6d59819f
= <lucasgualtieri.dev@gmail.com>
2024-03-20 15:46:41 -0300

    Leitura E Escrita em Java

---
+++ AEDs/Monitoria/Monitoria_AEDs_II/LeituraEEscrita.java
@@ -0,0 +1,43 @@
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+// clear && javac LeituraEEscrita.java && java LeituraEEscrita.java
+
+public class LeituraEEscrita {
+	public static void main(String[] args) throws Exception {
+
+		RandomAccessFile arquivo;
+		arquivo = new RandomAccessFile("teste.txt", "rw");
+
+		Scanner sc = new Scanner(System.in);
+		int qtdNumeros = sc.nextInt();
+
+		float real;
+		for (int i = 0; i < qtdNumeros; i++) {
+			// Leio do teclado
+			real = sc.nextFloat();
+			// Escrevo no Arquivo
+			arquivo.writeFloat(real);
+		}
+
+		long pos = arquivo.getFilePointer();
+		arquivo.seek(pos - 4);
+		
+		float resultado;
+		for (int i = 0; i < qtdNumeros; i++) {
+			// Salvo a posição inicial
+			pos = arquivo.getFilePointer();
+
+			// Leio do arquivo
+			resultado = arquivo.readFloat();
+
+			// Printo na tela
+			System.out.println(resultado);
+
+			// Voltar 4 bytes.
+			if (pos > 0) arquivo.seek(pos - 4);
+		}
+
+		arquivo.close();
+	}
+}
