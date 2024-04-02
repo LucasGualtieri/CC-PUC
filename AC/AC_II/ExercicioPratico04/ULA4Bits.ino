90419e039096c0b1056818977c6d6477af6b7cba
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-04-01 23:27:57 -0300

    AC - EP04

--- AC/AC_II/ExercicioPratico04/ULA4Bits.ino
+++ AC/AC_II/ExercicioPratico04/ULA4Bits.ino
@@ -6,8 +6,8 @@
 extern int __heap_start, *__brkval;
 
 int freeMemory() {
-  int v;
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+	int v;
+	return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 
 void throwException(String s) {
