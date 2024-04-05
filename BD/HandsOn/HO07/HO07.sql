5dd07e1b46614fa571b3def80a94b8dd65358be8
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-04-05 10:07:06 -0300

    BD

--- BD/HandsOn/HO07/HO07.sql
+++ BD/HandsOn/HO07/HO07.sql
@@ -5,9 +5,11 @@ SELECT first_name, last_name FROM actors WHERE gender = 'F'
 SELECT name FROM movies WHERE year > 1999
 
 -- Projetar o nome do filme e o nome do diretor de cada filme;
-SELECT first_name, last_name, name FROM
-movies_directors JOIN directors ON director_id = directors.id
-JOIN movies ON movie_id = movies.id
+-- INNER JOIN é sinônimo de JOIN
+SELECT first_name, last_name, name
+FROM   movies_directors
+JOIN   directors ON director_id = directors.id
+JOIN   movies ON movie_id = movies.id
 
 -- Projetar o nome do filme, nome do ator e o papel que cada ator teve no filme para filmes com ranking acima da nota 6;
 SELECT name, first_name, last_name, role, rank
@@ -26,8 +28,8 @@ SELECT first_name, last_name, COUNT(*) AS number_of_movies
 GROUP BY directors.first_name, directors.last_name;
 
 -- Projetar o gênero e o número de filmes de cada gênero; 
-SELECT genre, COUNT(*) AS number_of_movies
-FROM movies_genres
+SELECT   genre, COUNT(*) AS number_of_movies
+FROM     movies_genres
 GROUP BY genre;
 
 -- Projetar o gênero, o ranking (nota) médio, mínimo e máximo dos filmes do gênero.
@@ -35,6 +37,7 @@ SELECT genre,
 AVG(movies.rank) AS avg_rank,
 MIN(movies.rank) AS min_rank,
 MAX(movies.rank) AS max_rank
-FROM
-movies JOIN movies_genres ON movies.id = movie_id
+FROM (
+	movies JOIN movies_genres ON movies.id = movie_id
+)
 GROUP BY genre;
