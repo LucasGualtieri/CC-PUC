7f11ba9ec02795ea345ed0302c14c3fead17c6f0
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-04-22 21:20:47 -0300

    AEDs II - Monitoria

--- AEDs/Monitoria/Monitoria_AEDs_II/TP01Q02/Strsep.c
+++ AEDs/Monitoria/Monitoria_AEDs_II/TP01Q02/Strsep.c
@@ -2,28 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+// clear && gcc Strsep.c && ./a.out
+
 void main() {
-	// strdup foi necessário para o funcionamento do strdsep
-	// char* string = strdup("Lucas;;Gualtieri;Firace");
-	char* string = strdup("Lucas;;Gualtieri;Firace"); // Maloca
-	// const char string[] = "Lucas Gualtieri Firace";
+	char string[] = "Lucas;;Gualtieri;Firace";
+	char* aux = string;
 
 	char nome[50];
 	char segundoNome[50];
 	char terceiroNome[50];
 	char ultimonome[50];
 
-	strcpy(nome, strsep(&string, ";"));
-	char* temp = strsep(&string, ";");
+	strcpy(nome, strsep(&aux, ";"));
+	char* temp = strsep(&aux, ";");
 	strcpy(segundoNome, !strcmp(temp, "") ? "vazio" : temp);
-	strcpy(terceiroNome, strsep(&string, ";"));
-	strcpy(ultimonome, strsep(&string, ";"));
+	strcpy(terceiroNome, strsep(&aux, ";"));
+	strcpy(ultimonome, strsep(&aux, ";"));
 
 	printf("Nome: '%s'\n", nome);
 	printf("segundoNome: '%s'\n", segundoNome);
 	printf("terceiroNome: '%s'\n", terceiroNome);
 	printf("Ultimonome: '%s'\n", ultimonome);
-
-	free(string);
 
 }
