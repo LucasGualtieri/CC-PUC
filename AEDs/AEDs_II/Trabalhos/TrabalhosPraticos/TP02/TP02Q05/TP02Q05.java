ceb1a6b7f265f6ae798b088fbbadd1787c5b00ea
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-04-22 21:21:01 -0300

    TP05 Tunado!

--- AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/TP02Q05/TP02Q05.java
+++ AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/TP02Q05/TP02Q05.java
@@ -1,21 +1,27 @@
 package AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.TP02Q05;
 
 import AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.Libs.Lib;
-import AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.Libs.Lib.Lista;
 import AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.Libs.Lib.Log;
+import AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.Libs.Lib.Lista;
 import AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.Libs.Lib.Timer;
+import AEDs.AEDs_II.Trabalhos.TrabalhosPraticos.TP02.Libs.Lib.Jogador;
 
 // clear && javac -cp ../.. TP02Q05.java && java -cp ../.. TP02Q05.java < pub.in > result.txt
 
 class TP02Q05 {
 
-	static void SelectionSort(Log log, Lista lista) {
+	interface Comparable {
+		int compareTo(Jogador x, Jogador y);		
+	}
+
+	static void SelectionSort(Log log, Lista lista, Comparable c) {
 
 		int i, j, menor;
 		for (i = 0; i < lista.getSize() - 1; i++) {
 			menor = i;
 			for (j = i + 1; j < lista.getSize(); j++) {
-				if (lista.CompareToStr(menor, j, log) > 0) menor = j;
+				int compare = c.compareTo(lista.Get(menor), lista.Get(j));
+				if (compare > 0) menor = j;
 			}
 			lista.swap(i, menor, log);
 		}
@@ -29,7 +35,9 @@ class TP02Q05 {
 		Timer timer = new Timer();
 		Log log = new Log();
 
-		Lista BD = new Lista("../tmp/players.csv", BD_SIZE);
+		String path = "AEDs/AEDs_II/Trabalhos/TrabalhosPraticos/TP02/";
+
+		Lista BD = new Lista(path + "tmp/players.csv", BD_SIZE);
 		Lista lista = new Lista(465); // Tamanho de entradas do pri.in
 
 		String inputPUBIN = new String();
@@ -37,17 +45,20 @@ class TP02Q05 {
 		while (!(inputPUBIN = Lib.readStr()).equals("FIM")) {
 			int id = Integer.parseInt(inputPUBIN);
 			lista.Inserir(BD.Get(id));
-			lista.setAtributoStr(BD.Get(id).getNome());
 		}
 
+		Comparable c = (x, y) -> {
+			log.incrementarComparacoes();
+			int r = Integer.compare(x.getAnoNascimento(), y.getAnoNascimento());
+			return r != 0 ? r : x.getNome().compareTo(y.getNome());
+		};
+
 		timer.Start();
-		SelectionSort(log, lista);
+		SelectionSort(log, lista, c);
 		timer.Stop();
 
 		lista.Mostrar();
 
-		log.RegistroOrdenacao("794989_selecao.txt", timer);		
-
+		log.RegistroOrdenacao(path + "TP02Q05/794989_selecao.txt", timer);		
 	}
-
 }
