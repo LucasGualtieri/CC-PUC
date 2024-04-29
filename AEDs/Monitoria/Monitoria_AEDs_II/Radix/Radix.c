bebaa39c94ab0f1544dca584bff7ddb470a2b51e
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-04-29 19:36:04 -0300

    AEDs II - RadixSort

--- AEDs/Monitoria/Monitoria_AEDs_II/Radix/Radix.c
+++ AEDs/Monitoria/Monitoria_AEDs_II/Radix/Radix.c
@@ -8,20 +8,27 @@ char toUpper(char c) {
 
 void CountingSort(int place, Log* log, Lista* lista) {
 
+	char firstChar = '-';
+	char lastChar = 'F';
+	int ASCInterval = (lastChar - firstChar) + 1;
+
 	int N = lista->size;
 	String* array = lista->array;
 
-	// Aloca espaço para todos os possíveis valores de um Byte.
-	int* countArray = calloc(256, sizeof(int));
+	int* countArray = calloc(ASCInterval, sizeof(int));
 
-	for (int i = 0; i < N; i++) countArray[array[i][place]]++;
+	for (int i = 0; i < N; i++) {
+		char c = toUpper(array[i][place]);
+		countArray[c - firstChar]++;
+	}
 
-	for (int i = 1; i < 256; i++) countArray[i] += countArray[i - 1];
+	for (int i = 1; i < ASCInterval; i++) countArray[i] += countArray[i - 1];
 
 	String* auxArray = malloc(N * sizeof(String));
 
 	for (int i = N - 1; i >= 0; i--) {
-		char valor = array[i][place];
+		log->movimentacoes++;
+		char valor = toUpper(array[i][place]) - firstChar;
 		auxArray[--countArray[valor]] = array[i];
 	}
 
@@ -49,9 +56,6 @@ int main() {
 	Lista BD = newLista(0);
 	BD.ImportDataBase("ids.csv", &BD);
 
-	// BD.Mostrar(BD);
-
-	// printf("-----------------------------------------\n");
 	timer.Start(&timer);
 	RadixSort(&log, &BD);
 	timer.Stop(&timer);
@@ -59,7 +63,7 @@ int main() {
 	BD.Mostrar(BD);
 
 	bool result = BD.isSorted(BD);
-	printf("Ordenação funcionou? %s\n", result ? "SIM" : "NÃO");
+	printf("\nOrdenação funcionou? %s\n", result ? "SIM" : "NÃO");
 
 	log.RegistroOrdenacao(timer, log);
 
--- AEDs/Monitoria/Monitoria_AEDs_II/Radix/RadixSimplified.c
+++ AEDs/Monitoria/Monitoria_AEDs_II/Radix/RadixSimplified.c
@@ -2,33 +2,25 @@
 
 // clear && gcc Radix.c && ./a.out
 
-char toUpper(char c) {
-	return 'a' <= c && c <= 'z' ? c - 32 : c;
-}
+int GetMax(Lista* lista);
 
 void CountingSort(int place, Log* log, Lista* lista) {
-
-	char firstChar = '-';
-	char lastChar = 'F';
-	int ASCInterval = (lastChar - firstChar) + 1;
 
 	int N = lista->size;
 	String* array = lista->array;
 
-	int* countArray = calloc(ASCInterval, sizeof(int));
+	// Aloca espaço para todos os possíveis valores de um Byte.
+	int* countArray = calloc(256, sizeof(int));
 
-	for (int i = 0; i < N; i++) {
-		char c = toUpper(array[i][place]);
-		countArray[c - firstChar]++;
-	}
+	for (int i = 0; i < N; i++) countArray[array[i][place]]++;
 
-	for (int i = 1; i < ASCInterval; i++) countArray[i] += countArray[i - 1];
+	for (int i = 1; i < 256; i++) countArray[i] += countArray[i - 1];
 
 	String* auxArray = malloc(N * sizeof(String));
 
 	for (int i = N - 1; i >= 0; i--) {
 		log->movimentacoes++;
-		char valor = toUpper(array[i][place]) - firstChar;
+		char valor = array[i][place];
 		auxArray[--countArray[valor]] = array[i];
 	}
 
@@ -41,7 +33,7 @@ void CountingSort(int place, Log* log, Lista* lista) {
 
 void RadixSort(Log* log, Lista* lista) {
 
-	int max = 36; // Length da maior string
+	int max = GetMax(lista); // Length da maior string
 
 	for (int i = max - 1; i >= 0; i--) {
 		CountingSort(i, log, lista);
@@ -56,9 +48,6 @@ int main() {
 	Lista BD = newLista(0);
 	BD.ImportDataBase("ids.csv", &BD);
 
-	// BD.Mostrar(BD);
-
-	// printf("-----------------------------------------\n");
 	timer.Start(&timer);
 	RadixSort(&log, &BD);
 	timer.Stop(&timer);
@@ -66,9 +55,21 @@ int main() {
 	BD.Mostrar(BD);
 
 	bool result = BD.isSorted(BD);
-	printf("Ordenação funcionou? %s\n", result ? "SIM" : "NÃO");
+	printf("\nOrdenação funcionou? %s\n", result ? "SIM" : "NÃO");
 
 	log.RegistroOrdenacao(timer, log);
 
 	BD.Close(&BD);
+}
+
+int GetMax(Lista* lista) {
+
+	int max = strlen(lista->array[0]);
+
+	for (int i = 1; i < lista->size; i++) {
+		int valor = strlen(lista->array[i]);
+		if (valor > max) max = valor;
+	}
+
+	return max;
 }
