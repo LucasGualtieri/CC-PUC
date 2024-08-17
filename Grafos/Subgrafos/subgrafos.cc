14a3ea749d87b7da67b935b2ba9107fd1da195ed
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-08-17 00:35:08 -0300

    subgrafos

--- Grafos/Subgrafos/subgrafos.cc
+++ Grafos/Subgrafos/subgrafos.cc
@@ -1,31 +1,11 @@
 #include <cmath>
 #include <iostream>
-#include <vector>
+#include "../DataStructures/include/list/linearList.hpp"
+// #include <LinearList>
 
 using namespace std;
 
 // clear && g++ subgrafos.cc && ./a.out
-void vectorPrint(vector<int> vector) {
-
-	cout << "{ ";
-	for (int i : vector) cout << i << " ";
-	cout << "}" << endl;
-}
-void printarVertices(vector<vector<int>> cpVertices) { cout << "{ ";
-
-	for (vector<int> conjunto : cpVertices) {
-
-		cout << "{ ";
-
-		for (int v : conjunto) {
-			cout << v << " ";
-		}
-
-		cout << "}, ";
-	}
-
-	cout << "}";
-}
 
 int fat(int n) { return n <= 1 ? 1 : n * fat(n - 1); }
 
@@ -33,158 +13,64 @@ int np(int i, int n) {
 	return (fat(n)) / (fat(i) * (fat(n - i)));
 }
 
-vector<vector<int>> gerarCpVertices(vector<int> V) {
+template <typename T>
+LinearList<LinearList<T>> gerarConjuntoPotencia(LinearList<T> conjunto, size_t min = 0, size_t max = 0x7fffffff) {
 
-	int N = V.size();
+	if (max == 0x7fffffff) max = conjunto.size();
 
-	// vector<vector<int>> cpVertices(pow(2, N));
-	vector<vector<int>> cpVertices;
+	int N = conjunto.size();
 
-	for (int i = 1; i <= N; i++) { // i determina o tamanho do agrupamento
+	LinearList<LinearList<T>> conjuntoPotencia(pow(2, N));
 
-		vector<int> conjunto;
+	for (int i = min; i <= max; i++) { // i determina o tamanho do agrupamento
 
-		int setinha = 0;
-
-		for (int j = 0; j < np(i, N); j++) { // j determina a quantidade de agrupamentos
-
-			while (conjunto.size() < i) { // k determina o tamanho do conjunto que é = i
-
-				if (setinha >= N) {
-					if (conjunto[conjunto.size() - 1] == N) conjunto.pop_back();
-					setinha = conjunto[conjunto.size() - 1];
-					conjunto.pop_back();
-				}
-
-				conjunto.push_back(V[setinha++]);
-			}
-
-			cpVertices.push_back(conjunto);
-
-			conjunto.pop_back();
-		}
-	}
-
-	return cpVertices;
-}
-
-vector<vector<int>> gerarArestas(vector<int> V) {
-
-	// vectorPrint(V);
-
-	int N = V.size();
-
-	// vector<vector<int>> cpVertices(pow(2, N));
-	vector<vector<int>> arestas(1);
-
-	vector<int> conjunto;
-
-	int setinha = 0;
-
-	int i = 2;
-
-	// cout << "np: " << np(i, N) << endl;
-	// if (np(i, N) == 0) arestas.push_back(conjunto);
-
-	for (int j = 0; j < np(i, N); j++) { // j determina a quantidade de agrupamentos
-
-		while (conjunto.size() < i) { // k determina o tamanho do conjunto que é = i
-
-			if (setinha >= N) {
-				if (conjunto[conjunto.size() - 1] == N) conjunto.pop_back();
-				setinha = conjunto[conjunto.size() - 1];
-				conjunto.pop_back();
-			}
-
-			conjunto.push_back(V[setinha++]);
-		}
-
-		arestas.push_back(conjunto);
-
-		conjunto.pop_back();
-	}
-
-	return arestas;
-
-}
-
-/* void gerarCpArestas(vector<vector<int>> E, vector<vector<vector<int>>>& cpArestas) {
-
-	int N = E.size();
-
-	for (int i = 1; i <= N; i++) { // i determina o tamanho do agrupamento
-
-		vector<vector<int>> arestas;
+		LinearList<T> subconjunto(i);
 
 		int setinha = 0;
 
-		for (int j = 0; j < np(i, N); j++) {
+		int NP = np(i, N); // NP determina a quantidade de agrupamentos
 
-			while (arestas.size() < i) {
+		for (int j = 0; j < NP; j++) {
+
+			while (subconjunto.size() < i) {
 
 				if (setinha >= N) {
-					if (arestas[arestas.size() - 1] == N) arestas.pop_back();
-					setinha = arestas[arestas.size() - 1];
-					arestas.pop_back();
+					if (subconjunto.back() == conjunto.back()) subconjunto.pop_back();
+					setinha = conjunto.indexOf(subconjunto.pop_back()) + 1;
 				}
 
-				arestas.push_back(V[setinha++]);
+				subconjunto.push_back(conjunto[setinha++]);
 			}
 
-			cpVertices.push_back(arestas);
+			conjuntoPotencia.push_back(subconjunto);
 
-			arestas.pop_back();
+			if (!subconjunto.empty()) subconjunto.pop_back();
 		}
 	}
-} */
+
+	return conjuntoPotencia;
+}
 
 int main() {
 
-	const int N = 4;
+	const int N = 3;
 
-	// cout << fat(13) << endl;
-
-	vector<int> vertices(N);
-	for (int i = 1; i <= N; i++) vertices[i - 1] = i;
-
-	// for (int i : vertices) cout << i << " ";
-	// cout << endl;
-
-	vector<vector<int>> cpVertices = gerarCpVertices(vertices);
-	// printarVertices(cpVertices);
-
-	if (cpVertices.size() == pow(2, N) - 1) cout << "Deu bom a qtd" << endl;
-	// cout << endl << cpVertices.size() << endl;
+	LinearList<int> V(1, N);
 
 	int i = 1;
 
-	for (vector<int> V : cpVertices) {
+	for (LinearList<int> v : gerarConjuntoPotencia(V, 1)) {
 
-		vector<vector<int>> E = gerarArestas(V);
 
-		for (vector<int> aresta : E) {
+		LinearList<LinearList<int>> arestas = gerarConjuntoPotencia(v, 2, 2);
 
-			cout << "Subgrafo: " << i++ << endl;
-			cout << "V: ";
-			vectorPrint(V);
-			cout << "E: ";
-			vectorPrint(aresta);
+		LinearList<LinearList<LinearList<int>>> cpArestas = gerarConjuntoPotencia(arestas);
+
+		for (LinearList<LinearList<int>> aresta : cpArestas) {
+
+			cout << "Subgrafo " << i++ << ": " << endl;
+			cout << "V = " << v << endl;
+			cout << "E = " << aresta << endl;
 		}
-
-		// vector<vector<vector<int>>> cpArestas;
-
-		// cpArestas.push_back(E);
-
-		// gerarCpArestas(E, cpArestas);
-
-		// for () {
-
-			// cout << "Subgrafo: " << i++ << endl;
-			// cout << "V: ";
-			// vectorPrint(V);
-			// cout << "E: ";
-			// vectorPrint(aresta);
-		// }
 	}
 }
-
